f3cca0878658434990581cf22808c74dab6d815f
tokhichevsky <lerak01@yandex.ru>
2019-04-06 21:29:02 +0300

    Реализация модуля окончена

--- Ecosystem-of-information-bots/news_collector_bot/main.py
+++ Ecosystem-of-information-bots/news_collector_bot/main.py
@@ -1,5 +1,1 @@
-import vk
 
-
-def get_last_news(token, public, last_publication):
-    return 0
---
+++ Ecosystem-of-information-bots/news_collector_bot/modules/vkcollector/collector.py
@@ -0,0 +1,31 @@
+from vk_api import vk_api
+
+
+def get_token(address):
+    f = open(address, 'r')
+    token = f.read()
+    f.close()
+    return token
+
+
+def _get_last_news(vk: vk_api.VkApiMethod, public: str, last_publication: int, batch: int =5):
+    last_news = []
+    offset = 0
+    while(True):
+        response = list(filter(lambda x: x['date'] > last_publication,
+                               vk.wall.get(domain=public, count=batch, offset=offset)['items']))
+        last_news.extend(response)
+        if len(response) < batch:
+            break
+    return last_news
+
+
+def get_last_news(token: str, public: str, last_publication: int, batch: int =5):
+    vk_session = vk_api.VkApi(token=token)
+    try:
+        vk_session._auth_token(reauth=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+    vk = vk_session.get_api()
+    return _get_last_news(vk, public, last_publication, batch)
