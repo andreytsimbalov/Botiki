e75e17744cf1bec2256a1792cae3f9136aa5ada8
tokhichevsky <lerak01@yandex.ru>
2019-04-09 17:52:50 +0300

    Добавлена функция авторизации

--- projects/news_collector_bot/modules/vkcollector/collector.py
+++ projects/news_collector_bot/modules/vkcollector/collector.py
@@ -17,38 +17,12 @@ def get_token(address: str) -> str:
     return token
 
 
-def _get_last_news(vk: vk_api.VkApiMethod, public: str, last_publication: int, batch: int):
+def auth(token: str):
     """
-    Находит последние посты
+    Авторизуется в системе Vk
 
-    :param vk: авторизованная сессия
-    :param public: domain паблика
-    :param last_publication: unix время последней известной боту публикации
-    :param batch: число новостей, проверяемых за раз
-    :return: list новостей позже указанного last_publication
-    """
-    last_news = []
-    offset = 0
-    while(True):
-        response = list(filter(lambda x: x['date'] > last_publication,
-                               vk.wall.get(domain=public, count=batch, offset=offset)['items']))
-        last_news.extend(response)
-        if len(response) < batch:
-            break
-        offset += batch
-    return last_news
-
-
-def get_last_news(token: str, public: str, last_publication: int, batch: int = 5, with_last_publ_time: bool = True):
-    """
-    Общая функция, собирающая последние посты из указанного паблика.
-
-    :param str token: токен
-    :param str public: domain паблика
-    :param int last_publication: unix время последней известной боту публикации
-    :param int batch: число новостей, проверяемых за раз (default: 5)
-    :param bool with_last_publ_time: возвращать unix время свежайшей новости? (default: True)
-    :return: list новостей позже указанного last_publication
+    :param token: токен
+    :return: авторизованная сессия vk_api.VkApi
     """
     vk_session = vk_api.VkApi(token=token)
     try:
@@ -57,7 +31,31 @@ def get_last_news(token: str, public: str, last_publication: int, batch: int = 5
         print(error_msg)
         return
     vk = vk_session.get_api()
-    last_news = _get_last_news(vk, public, last_publication, batch)
+    return vk
+
+
+def get_last_news(vk: vk_api.VkApiMethod, public: str, last_publication: int,
+                  batch: int = 5, with_last_publ_time: bool = True):
+    """
+    Функция, собирающая последние посты из указанного паблика.
+
+    :param vk: авторизованная сессия
+    :param str public: domain паблика
+    :param int last_publication: unix время последней известной боту публикации
+    :param int batch: число новостей, проверяемых за раз (default: 5)
+    :param bool with_last_publ_time: возвращать unix время свежайшей новости? (default: True)
+    :return: list новостей позже указанного last_publication
+    """
+
+    last_news = []
+    offset = 0
+    while (True):
+        response = list(filter(lambda x: x['date'] > last_publication,
+                               vk.wall.get(domain=public, count=batch, offset=offset)['items']))
+        last_news.extend(response)
+        if len(response) < batch:
+            break
+        offset += batch
     if (with_last_publ_time):
         return last_news, last_news[0]['date']
     else:
