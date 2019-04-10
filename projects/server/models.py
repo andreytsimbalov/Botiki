a776bc55ef7068a7d51291602e7842842f30cde7
July <nikirova.july@gmail.com>
2019-04-10 20:49:19 +0300

    BD changes

--- projects/server/models.py
+++ projects/server/models.py
@@ -1,55 +1,48 @@
 import datetime
-from peewee import *
-bot_db = PostgresqlDatabase(
-    'my_bot',
-    user='postgres',
-    password='',
-    host='localhost',
-)
+from django.db import models
 
 
-class BaseModel(Model):
-    class Meta:
-        database = bot_db
+class User(models.Model):
+    id = models.CharField(primary_key=True)  # taken from vk, and could be used to make link
+    name = models.CharField(max_length=40)
+    # only three options
+    FREQUENCY = (
+        ('I', 'Immediately'),
+        ('D', 'Daily'),
+        ('W', 'Weekly'),
+    )
+    frequency = models.CharField(max_length=1, choices=FREQUENCY)
+    last.upd = models.DateTimeField(default=datetime.datetime.now)  # last time user has got news
 
 
-class User(BaseModel):
-    id = CharField(primary_key=True)  # taken from vk, and could be used to make link
-    name = CharField()
-    frequency = CharField(default='immediately')
+class Group(models.Model):
+    id = models.int(primary_key=True)
+    name = models.CharField(max_length=40)
+    link = models.FixedCharField(unique=True)
 
 
-class Group(BaseModel):
-    id = int(primary_key=True)
-    name = CharField()
-    link = FixedCharField(unique=True)
+class Tag(models.Model):
+    id = models.int(primary_key=True)
+    name = models.CharField(max_length=20)
 
 
-class Tag(BaseModel):
-    id = int(primary_key=True)
-    name = CharField()
+class Post(models.Model):
+    id = models.CharField(primary_key=True)  # taken from vk, and could be used to make link
+    name = models.CharField()
+    content = models.TextField()
+    pub_date = models.DateTimeField(default=datetime.datetime.now)
+    group_id = models.ForeignKeyField(Group)
+    tags = models.ManyToManyField(Tag)
 
 
-class Post(BaseModel):
-    id = CharField(primary_key=True)  # taken from vk, and could be used to make link
-    name = CharField()
-    content = TextField()
-    pub_date = DateTimeField(default=datetime.datetime.now)
-    group_id = ForeignKeyField(Group)
+class PostAttachment(models.Model):
+    post_id = models.ForeignKeyField(Post)
+    attachment = models.CharField()
 
 
-class PostAttachment(BaseModel):
-    post_id = ForeignKeyField(Post)
-    attachment = CharField()
-
-
-class PostTag(BaseModel):
-    post_id = ForeignKeyField(Post)
-    tag_id = ForeignKeyField(Tag)
-
-
-class UserGroup(BaseModel):
-    user_id = ForeignKeyField(User)
-    group_id = ForeignKeyField(Group)
-    tag_id = ForeignKeyField(Tag)
-    spec_freq = CharField(default=None)
+# for each group each user has a number of tags
+class UserGroup(models.Model):
+    user_id = models.ForeignKeyField(User)
+    group_id = models.ForeignKeyField(Group)
+    tag_id = models.ForeignKeyField(Tag)
+    spec_freq = models.CharField(default=None)
