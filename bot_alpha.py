92938609d97bff3e49c72640ad9cdd9746297e86
Леонид <romanychev-l@users.noreply.github.com>
2019-04-15 02:32:14 +0300

    fix

--- bot_alpha.py
+++ bot_alpha.py
@@ -7,11 +7,12 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from peewee import *
 import os
 
-
 full_path = os.path.realpath(__file__)
 path, filename = os.path.split(full_path)
+time_flag = datetime.datetime.now()
 
 db = SqliteDatabase(path + '/database.db')
+
 
 class Article(Model):
     title = CharField()
@@ -20,15 +21,18 @@ class Article(Model):
     class Meta:
         database = db
 
+
 def init():
     db.create_tables([Article])
 
 
 def add_article(title, link):
-    Article.create(title = title, link = link)
+    Article.create(title=title, link=link)
+
 
 def get_all_article():
     return Article.select()
+
 
 interval = 5
 
@@ -46,13 +50,17 @@ longpoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-        vk.messages.send(user_id=event.user_id, message='Батя, я работаю', random_id=random.random())
+        vk.messages.send(user_id=event.user_id, message='Ваша подборка на сегодня, Господин', random_id=random.random())
 
         articles = get_all_article()
 
         i = 0
+        mess = ''
         for art in articles:
-            vk.messages.send(user_id=event.user_id, message=str(art.id) + '\n' + art.title + '\n' + art.link, random_id=random.random())
-            i += 1
-            if(i > 5):
+            mess += str(art.id) + '. ' + art.title + '\n' + art.link + '\n' + '\n'
+
+            if(i > 9):
                 break
+            i += 1
+
+        vk.messages.send(user_id=event.user_id, message=mess, random_id=random.random())
